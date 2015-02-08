c278ee793b8f777a235b867751aa26d826d552b0
dumtara <dumtara@users.noreply.github.com>
2015-02-08 15:31:12 +0530

    Update vendorsetup.sh
    add_lunch_combo aokp_apexqtmo-userdebug

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -59,3 +59,4 @@ add_lunch_combo aokp_v500-userdebug
 add_lunch_combo aokp_vs920-userdebug
 add_lunch_combo aokp_vs980-userdebug
 add_lunch_combo aokp_yuga-userdebug
+add_lunch_combo aokp_tomato-userdebug
